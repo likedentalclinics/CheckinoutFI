f1510b087706350e26ffe405fb1ff281c6d69af4
likedentalclinics <tgstrider@hotmail.com>
2025-04-04 14:14:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbzrb8BhZC67fbYaV
 const CONDUCT_URL = 'https://script.google.com/macros/s/AKfycbx4j3K7OohIlcHmz47e2AqMRpetuFEoMdm_UvcnLMMYkVARuIwSSoBV_Tthyju2yzX3/exec';
 
 //LIFF_ID
-const LIFF_ID = ''; // LIFF ID Checkin
-const LIFF_ID1 = ''; // LIFF ID history
-const LIFF_ID2 = ''; // LIFF ID conduct
-const LIFF_ID3 = ''; // LIFF ID ot
-const LIFF_ID4 = ''; // LIFF ID register
+const LIFF_ID = '2007205084-WPoj0jZd'; // LIFF ID Checkin
+const LIFF_ID1 = '2007205084-WbvGQGp2'; // LIFF ID history
+const LIFF_ID2 = '2007205084-o44yKyvG'; // LIFF ID conduct
+const LIFF_ID3 = '2007205084-9kkJaJW2'; // LIFF ID ot
+const LIFF_ID4 = '2007205084-odRBwBAL'; // LIFF ID register
